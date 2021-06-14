a09d8ee95751e1e69a298b4ab786f1d5dadaa468
Willian Souza <84147549+KyanityTnT@users.noreply.github.com>
2021-06-14 17:50:14 -0300

    Primeira Parte Projeto Api C-sharp

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using GerenciadorDeTarefas.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+
+        private readonly ILogger<LoginController> _logger;
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto requisicao)
+        {
+            try
+            {
+                if(requisicao == null || requisicao.Login == null || requisicao.Senha == null)
+                {
+                    return BadRequest(new ErroRespostaDto()
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Erro = " Parametros de entrada inválidos"
+                    });
+                }
+
+                return Ok("usuário autenticado com sucesso");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Ocurreu erro a efetuar login:{ e.Message}", e);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespostaDto()
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Erro = " Ocorreu erro ao efetuar login, tente novamente!"
+                });
+
+
+            }
+        }
+    }
+}
---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Dtos/ErroRespostaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Dtos
+{
+    public class ErroRespostaDto
+    {
+
+        public int Status { get; set; }
+        public string Erro { get; set; }
+
+    }
+}
